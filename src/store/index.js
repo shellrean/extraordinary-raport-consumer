a8a6939a097ee8a1566ded8342da08b9fc07067c
shellrean <reanyusufali@gmail.com>
2020-12-31 17:27:22 +0700

    :heavy_plus_sign: added: authentication system with access & refresh token

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import auth from './auth'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -8,18 +10,39 @@ export default new Vuex.Store({
     errors: [],
     isLoading: false,
     loadPage: false,
-    token: localStorage.getItem('token'),
+    access_token: localStorage.getItem('access_token'),
+    refresh_token: localStorage.getItem('refresh_token'),
     baseURL: process.env.VUE_APP_URL
   },
   getters: {
     isAuth: state => {
-      return state.token != "null" && state.token != null
+      return state.access_token != "null" && state.access_token != null
     },
   },
   mutations: {
-  },
-  actions: {
+    _set_access_token(state, payload) {
+      state.access_token = payload
+    },
+    _set_refresh_token(state, payload) {
+      state.refresh_token = payload
+    },
+    _set_errors(state, payload) {
+      state.errors = payload
+    },
+    _clear_errors(state) {
+      state.errors = []
+    },
+    _loading_page(state, payload) {
+      state.loadPage = payload
+    },
+    _set_loading(state, payload) {
+      state.isLoading = payload
+    },
+    _set_baseurl(state, payload) {
+      state.baseURL = payload
+    }
   },
   modules: {
+    auth
   }
 })
