329f688a738c2e157009fca11c262410bcb25b9f
shellrean <reanyusufali@gmail.com>
2020-12-31 17:28:03 +0700

    :heavy_plus_sign: added: send payload request to server

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -8,13 +8,14 @@
             <div class="card-body">
               <h2>Login</h2>
               <p class="text-muted">Masuk akun anda</p>
+              <form @submit.prevent="postLogin">
               <div class="input-group mb-3">
                 <div class="input-group-prepend">
                   <span class="input-group-text">
                     <i class="flaticon-email"></i>
                   </span>
                 </div>
-                <input class="form-control" type="text" placeholder="Email" name="username" required autofocus>
+                <input class="form-control" type="email" placeholder="Email" name="username" required autofocus v-model="data.email">
               </div>
               <div class="input-group mb-4">
                 <div class="input-group-prepend">
@@ -22,9 +23,10 @@
                     <i class="flaticon-lock"></i>
                   </span>
                 </div>
-                <input class="form-control" placeholder="Password" />
+                <input class="form-control" placeholder="Password" required v-model="data.password"/>
               </div>
               <button class="btn btn-primary px-4" type="submit" :disabled="isLoading">Login</button>
+              </form>
             </div>
           </div>
           <div class="card text-white bg-primary py-5 d-md-down-none" style="width:44%">
@@ -42,7 +44,7 @@
   </div>
 </template>
 <script>
-import { mapActions, mapMutations, mapGetters, mapState } from 'vuex'
+import { mapActions, mapGetters, mapState } from 'vuex'
 export default {
   data() {
     return {
@@ -64,13 +66,17 @@ export default {
     }) 
   },
   methods: {
+    ...mapActions('auth', ['submit']),
     async postLogin() {
       try {
-
+        let provider = await this.submit(this.data)
+        if (this.isAuth) {
+          this.$router.push({ name: 'dashboard' })
+        }
       } catch (error) {
         this.$bvToast.toast(`Error: ${error.message}`, {
           title: "Terjadi kesalahan",
-          variant: "success",
+          variant: "danger",
           solid: true
         })
       }
