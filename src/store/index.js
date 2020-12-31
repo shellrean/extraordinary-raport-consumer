b2e88fd63b46ca1ec8095173a1de327212b0f5c3
shellrean <reanyusufali@gmail.com>
2020-12-31 22:00:37 +0700

    :heavy_plus_sign: added: module user

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from './auth'
+import user from './user'
 
 Vue.use(Vuex)
 
@@ -43,6 +44,7 @@ export default new Vuex.Store({
     }
   },
   modules: {
-    auth
+    auth,
+    user
   }
 })
