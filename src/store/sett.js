d3a0cf14dc0fb165bf79c99014f3f61bfd3c9021
shellrean <reanyusufali@gmail.com>
2021-01-12 05:56:01 +0700

    :heavy_plus_sign: add: sett store set

---
+++ src/store/sett.js
@@ -0,0 +1,20 @@
+import $axios from '@/core/services/api.service.js'
+
+const state = () => ({
+
+})
+
+const mutations = {
+
+}
+
+const actions = [
+
+]
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+}
