430a615ec55e33d372b2907409c3175fcde53fc9
shellrean <reanyusufali@gmail.com>
2021-01-20 17:22:44 +0700

    :heavy_plus_sign: add: ClassroomAcademic component

---
+++ src/views/academic/classroom/Index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container md:mx-auto flex flex-col justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+    <div class="w-full lg:w-3/4 lg:py-4 lg:px-4">
+      <div class="bg-white py-2 px-2 border border-gray-300 shadow rounded-t-lg mb-2">
+        <div class="flex justify-end space-x-2 mb-2">
+          <div class="w-4 h-4 bg-red-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></div>
+          <div class="w-4 h-4 bg-yellow-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></div>
+          <div class="w-4 h-4 bg-green-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></div>
+        </div>
+        <div class="flex space-x-4">
+          <div class="w-12 bg-gray-200">
+            
+          </div>
+          <div class="flex-1">
+            <input type="text" class="py-1 rounded-full px-6 bg-gray-100 w-full focus:ring-2" placeholder="Cari"/>
+          </div>
+        </div>
+      </div>
+      <div class="max-h-96 overflow-y-scroll">
+        <div class="bg-white mb-2 border border-gray-300 shadow rounded-lg transition duration-500 hover:shadow-xl cursor-pointer" v-for="classroom in classrooms">
+          <div class="flex flex-col sm:flex-row py-2 sm:justify-between sm:items-center">
+            <div>
+              <div class="px-3 font-semibold text-gray-700">V11-A - 11</div>
+              <span class="px-3 text-gray-500 text-sm">Teknik Komputer Jaringan</span>
+            </div>
+            <span class="px-3 text-gray-500 text-sm">Prof.Dr. Shellrean. ST</span>
+          </div>
+        </div>
+      </div>
+      <div class="bg-white py-2 px-2 border border-gray-300 shadow rounded-b-lg mb-2">
+        <router-link :to="{name: 'academic.classroom.create'}" class="py-1 px-4 border-gray-300 border bg-gray-100 text-gray-600 rounded-md">Buat kelas akademik</router-link>
+      </div>
+    </div>
+    <div class="absolute left-4 bottom-4 xl:left-10 xl:bottom-10">
+      <div class="flex flex-col space-y-2">
+        <Notif :msg="val.msg" v-for="val in notif" :key="val.id"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Notif from '@/components/nano/Notif'
+import Notify from '@/core/services/notif.service'
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'AcademicClassroom',
+  components: {
+    Notif,
+  },
+  data() {
+    return {
+      notif: [],
+    }
+  },
+  computed: {
+    ...mapState(['isLoading']),
+    ...mapState('academic_classroom', ['classrooms']),
+  },
+  methods: {
+    ...mapActions('academic_classroom', ['fetchClassrooms']),
+    fetchData() {
+      (async() => {
+        try {
+          await this.fetchClassrooms()
+        } catch (err) {
+          Notify.createNotif({msg: err.message, notif: this.notif})
+        }
+      })()
+    }
+  },
+  created() {
+    this.fetchData()
+  }
+};
+</script>
