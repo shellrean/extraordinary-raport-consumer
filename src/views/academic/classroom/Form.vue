a79c009b6c22c8c8e9ea0e08018a203dafd94dbd
shellrean <reanyusufali@gmail.com>
2021-01-20 17:22:18 +0700

    :heavy_plus_sign: add: Form component for classroom_academic

---
+++ src/views/academic/classroom/Form.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container md:mx-auto flex flex-col justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+    <div class="bg-white w-full lg:w-1/2 py-4 px-4 rounded-lg shadow border border-gray-300">
+      <p class="text-gray-500 text-xl mb-2 font-medium">Kelas Akademik</p>
+      <form @submit.prevent="() => true">
+        <ClassroomForm />
+        <router-link :to="{name: 'academic.classroom' }" class="inline-block mr-1 rounded-md py-1 px-3 bg-gray-400 border-2 border-gray-500 hover:shadow-xl disabled:opacity-50 text-white font-medium">Kembali</router-link>
+        <button type="submit" class="py-1 px-3 rounded-md bg-green-400 border-2 border-green-500 hover:shadow-xl disabled:opacity-50 text-white font-medium">{{ isLoading ? 'Processing...' : 'Simpan' }}</button> 
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import ClassroomForm from './ClassroomForm'
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'AcademicClassroomForm',
+  components: [
+    ClassroomForm,
+  ],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapState(['isLoading'])
+  }
+}
+</script>
