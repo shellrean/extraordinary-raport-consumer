5ce2c281aec4df9c0691e494facacff30b2d4023
shellrean <reanyusufali@gmail.com>
2021-01-20 17:21:13 +0700

    :heavy_plus_sign: add: AcademicIndex component

---
+++ src/views/academic/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col space-y-4 lg:flex-row lg:space-y-0 h-full w-full">
+    <div class="border h-full w-full lg:flex-1 px-3 min-h-0 min-w-0">
+      <div class="max-w-md flex justify-center items-center h-full mx-auto">
+        <AcademicMenus />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AcademicMenus from '@/components/AcademicMenus'
+export default {
+  components: {
+    AcademicMenus
+  }
+};
+</script>
