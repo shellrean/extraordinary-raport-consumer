856185ca90eedafa7cb33b1c58ea733295fea7af
shellrean <reanyusufali@gmail.com>
2021-01-21 09:21:27 +0700

    :heavy_plus_sign: add: fetchSettings method to sett store

--- src/store/sett.js
+++ src/store/sett.js
@@ -1,16 +1,48 @@
 import $axios from '@/core/services/api.service.js'
+import Message from '@/core/domain/message.domain.js'
 
 const state = () => ({
-
+    settings: []
 })
 
 const mutations = {
-
+    _assign_setting_data(state, payload) {
+        state.settings = payload
+    }
 }
 
-const actions = [
+const actions = {
+    fetchSettings({ commit }, names = []) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                commit('_set_loading', true, { root: true })
+                let network = await $axios.get(`settings/?q=${names.join(",")}`)
 
-]
+                if (network.data.success) {
+                    commit('_assign_setting_data', network.data.data)
+                }
+
+                commit('_set_loading', false, { root: true })
+                resolve(network.data)
+            } catch (error) {
+                if (typeof error.response != 'undefined') {
+                    if (typeof error.response.data != 'undefined') {
+                        if (typeof error.response.data.error_code != 'undefined') {
+                            reject({message: error.response.data.message})
+                        } else {
+                            reject(Message.ErrUnExHappen)
+                        }
+                    } else {
+                        reject(Message.ErrUnExHappen)
+                    }
+                } else {
+                    reject(Message.ErrNotSendRequest)
+                }
+                commit('_set_loading', false, { root: true })
+            }
+        })
+    }
+}
 
 export default {
     namespaced: true,
