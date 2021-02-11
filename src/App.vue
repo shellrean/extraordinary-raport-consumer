ff43c5b4a164a8e481bcab714a030cb7ee9b78fd
shellrean <reanyusufali@gmail.com>
2021-02-11 13:30:33 +0700

    :x: remove: unused code

--- src/App.vue
+++ src/App.vue
@@ -7,34 +7,6 @@ import Message from '@/core/domain/message.domain'
 import { mapState, mapActions, mapGetters } from 'vuex'
 export default {
   name: 'App',
-  // computed: {
-  //   ...mapState(['isLoading']),
-  //   ...mapGetters(['isAuth']),
-  // },
-  // methods: {
-  //   ...mapActions('auth', ['getAuthorizeUser']),
-  //   showError(err) {
-  //     const error = new Message(err)
-  //     const message = error.getMessage()
-  //     const code = error.getCode()
-  //     const notification = new Notify(code, message)
-  //     notification.sweetAlertNotif(this)
-  //   },
-  //   getUserAuthorize() {
-  //     (async() => {
-  //       try {
-  //         await this.getAuthorizeUser()
-  //       } catch (err) {
-  //         this.showError(err)
-  //       }
-  //     })()
-  //   },
-  // },
-  // created() {
-  //   if (this.isAuth) {
-  //     this.getUserAuthorize()
-  //   }
-  // }
 };
 </script>
 <style type="scss">
