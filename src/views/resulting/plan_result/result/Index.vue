acde7d1db73fe96eb84e40a28534bba56606e9c8
shellrean <reanyusufali@gmail.com>
2021-02-15 15:07:00 +0700

    :heavy_plus_sign: add: support for view data-tree subject-plan-result

--- src/views/resulting/plan_result/result/Index.vue
+++ src/views/resulting/plan_result/result/Index.vue
@@ -1,31 +1,21 @@
 <template>
   <div class="container md:mx-auto flex flex-col justify-center space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
     <div class="w-full lg:w-3/4 lg:py-4 lg:px-4">
-      <div class="bg-white py-2 px-2 border-t border-gray-300 shadow rounded-t-lg">
+      <div class="bg-white py-2 px-2 border-t shadow border-gray-300 rounded-t-lg">
         <div class="flex justify-end space-x-2 mb-2">
-          <router-link :to="{name:'resulting.plan.result'}" class="block w-4 h-4 bg-red-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></router-link>
+          <router-link :disabled="typeof currentData.classroomAcademicID == 'undefined'" :to="{name:'resulting.plan.result.subject', params: {id: currentData.classroomAcademicID}}" class="block w-4 h-4 bg-red-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></router-link>
           <div class="w-4 h-4 bg-yellow-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></div>
           <div class="w-4 h-4 bg-green-400 rounded-full cursor-pointer hover:shadow-xl hover:w-8 hover:h-8"></div>
         </div>
-        <div class="flex space-x-1 items-center mb-4">
-          <div class="flex px-3 w-56">
-            <select class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" placeholder="Nomor Induk Siswa Nasional" :readonly="isLoading" :class="{'bg-gray-200' : isLoading}">
-              <option value="task">Tugas</option>
-              <option value="evms">Evaluasi Tengah Semester</option>
-              <option value="evls">Evaluasi Akhir Semester</option>
-              <option value="exam">Ulangan</option>
-            </select> 
-          </div>
-        </div>
         <div class="flex mb-4">
-          <p class="pb-2 border-b-2 px-4 font-semibold cursor-pointer" :class="pageIndex == 0 ? 'border-blue-400 text-blue-400' : 'border-gray-200 text-gray-400'" @click="pageIndex=0">Tabel</p>
-          <p class="pb-2 border-b-2 px-4 font-semibold cursor-pointer " :class="pageIndex == 1 ? 'border-blue-400 text-blue-400' : 'border-gray-200 text-gray-400'" @click="pageIndex=1">Export</p>
-          <p class="pb-2 border-b-2 border-gray-200 px-4 flex-1"></p>
+          <p class="pb-2 border-b-2 px-4 font-semibold cursor-pointer" :class="pageIndex == 0 ? 'border-blue-400 text-blue-400' : 'border-gray-300 text-gray-400'" @click="pageIndex=0">Tabel</p>
+          <p class="pb-2 border-b-2 px-4 font-semibold cursor-pointer " :class="pageIndex == 1 ? 'border-blue-400 text-blue-400' : 'border-gray-300 text-gray-400'" @click="pageIndex=1">Export</p>
+          <p class="pb-2 border-b-2 border-gray-300 px-4 flex-1"></p>
         </div>
       </div>
       <tabel-result v-if="pageIndex == 0"></tabel-result>
       <export-result v-if="pageIndex == 1"></export-result>
-      <div class="bg-white py-2 px-2 border border-gray-300 shadow rounded-b-lg mb-2">
+      <div class="bg-white py-2 px-2 border border-gray-300 rounded-b-lg mb-2">
       </div>
     </div>
   </div>
@@ -34,6 +24,8 @@
 import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
 import TabelResult from './Tabel'
 import ExportResult from './Export'
+import { showSweetError } from '@/core/helper/alert.helper'
+import { mapState, mapActions } from 'vuex'
 export default {
   components: {
     PerfectScrollbar,
@@ -42,7 +34,83 @@ export default {
   },
   data() {
     return {
-      pageIndex: 0
+      pageIndex: 0,
+      currentData: {},
+    }
+  },
+  computed: {
+    ...mapState(['isLoading']),
+    ...mapState('academic_subject', ['subjects']),
+    ...mapState('academic_student', ['students']),
+    ...mapState('subject_plan', ['plans']),
+    ...mapState('plan_result', ['results'])
+  },
+  methods: {
+    ...mapActions('academic_student', ['fetchStudentsByClassroomAcademic']),
+    ...mapActions('subject_plan', ['fetchSubjectPlans']),
+    ...mapActions('academic_subject', ['fetchSubjects']),
+    showError(err) {
+      showSweetError(this, err)
+    },
+    fetchDataStudentByCA() {
+      (async() => {
+        try {
+          await this.fetchStudentsByClassroomAcademic(this.currentData.classroomAcademicID)
+        } catch (err) {
+          if (typeof err.error_code != 'undefined' && err.error_code == 1201) {
+            this.fetchDataStudentByCA()
+            return
+          }
+          this.showError(err)
+        }
+      })()
+    },
+    fetchDataSubjectPlans() {
+      (async() => {
+        try {
+          await this.fetchSubjectPlans({
+            classroomID: this.currentData.classroomAcademicID
+          })
+        } catch (err) {
+          if (typeof err.error_code != 'undefined' && err.error_code == 1201) {
+            this.fetchDataSubjectPlans()
+            return
+          }
+          this.showError(err)
+        }
+      })()
+    },
+    setCurrentData() {
+      if (this.subjects == null || this.subjects == undefined) {
+        return
+      }
+      const result = this.subjects.filter((item) => item.id == parseInt(this.$route.params.id))
+      if (result.length > 0) {
+        this.currentData = result[0]
+      }
+    },
+    fetchDataSubjects() {
+      (async () => {
+        try {
+          await this.fetchSubjects()
+          this.setCurrentData()
+        } catch (err) {
+          if (typeof err.error_code != 'undefined' && err.error_code == 1201) {
+            this.fetchDataSubjects()
+            return
+          }
+          this.showError(err)
+        }
+      })()
+    }
+  },
+  created() {
+    this.fetchDataSubjects()
+  },
+  watch: {
+    currentData() {
+      this.fetchDataStudentByCA()
+      this.fetchDataSubjectPlans()
     }
   }
 }
--- src/views/resulting/plan_result/result/Tabel.vue
+++ src/views/resulting/plan_result/result/Tabel.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="bg-white border-gray-300 shadow">
+    <div class="flex items-center space-x-1 px-3 w-64 mb-4">
+      <select class="w-full pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-blue-300" placeholder="Nomor Induk Siswa Nasional" :readonly="isLoading" :class="{'bg-gray-200' : isLoading}" v-model="planIndex">
+        <option v-for="(plan, index) in plans" :value="index">{{ plan.name }}</option>
+      </select> 
+    </div>
     <div class="px-3">
       <perfect-scrollbar class="max-h-96 overflow-y-scroll">
       <table class="w-full ">
         <tbody>
-          <tr  class="border-b rounded-md border-gray-100 font-semibold text-white">
-            <td class="bg-blue-400 py-2 px-2 rounded-tl-md">No</td>
-            <td class="bg-blue-400 py-2 px-2">NIS</td>
-            <td class="bg-blue-400 py-2 px-2">Nama</td>
-            <td class="bg-blue-400 py-2 px-2 rounded-tr-md">Nilai</td>
+          <tr  class="bg-blue-100 border-b-2 border-t-2 border-blue-300 font-semibold text-blue-600">
+            <td class="py-2 px-2">No</td>
+            <td class="py-2 px-2">NIS</td>
+            <td class="py-2 px-2">Nama</td>
+            <td class="py-2 px-2 w-16" v-if="typeof planSelected.countPlan != 'undefined'" v-for="(_, i) in new Array(planSelected.countPlan)">Nilai {{ i+1 }}</td>
           </tr>
-          <tr class="hover:bg-blue-50 border-b border-gray-200 font-medium text-gray-600" v-for="(i, index) in new Array(36)">
+          <tr class="hover:bg-blue-50 border-b border-gray-200 font-medium text-gray-600" v-for="(student, index) in students" :key="student.id">
             <td class="py-1 px-2">{{ index+1 }}</td>
-            <td class="py-1 px-2">912020129</td>
-            <td class="py-1 px-2">Muhammad Ilham Nasution</td>
-            <td class="py-1 px-2">
-              <p class="font-semibold">100</p>
+            <td class="py-1 px-2">{{ student.studentSRN }}</td>
+            <td class="py-1 px-2">{{ student.studentName }}</td>
+            <td class="py-1 px-2" v-if="typeof planSelected.countPlan != 'undefined'" v-for="(_, i) in new Array(planSelected.countPlan)" :key="i">
+              <p class="font-semibold">{{ getNumberValue(student.id, i) }}</p>
             </td>
           </tr>
         </tbody>
@@ -26,13 +31,55 @@
 </template>
 <script>
 import { PerfectScrollbar } from 'vue2-perfect-scrollbar'
+import { mapState, mapActions } from 'vuex'
+import { showSweetError } from '@/core/helper/alert.helper'
 export default {
   components: {
     PerfectScrollbar
   },
   data() {
     return {
-      pageIndex: 0
+      planIndex: null,
+      planSelected: {},
+      showInfo: false,
+      value: {}
+    }
+  },
+  computed: {
+    ...mapState(['isLoading']),
+    ...mapState('academic_student', ['students']),
+    ...mapState('subject_plan', ['plans']),
+    ...mapState('plan_result', ['results'])
+  },
+  methods: {
+    ...mapActions('plan_result', ['fetchPlanResults']),
+    fetchDataPlanResults() {
+      (async() => {
+        try {
+          await this.fetchPlanResults(this.planSelected.id)
+        } catch (err) {
+          if (typeof err.error_code != 'undefined' && err.error_code == 1201) {
+            this.fetchDataPlanResults(this.planSelected.id)
+            return
+          }
+          this.showError(err)
+        }
+      })()
+    },
+    getNumberValue(studentID, index) {
+      if (this.results == null || typeof this.results == 'undefined') {
+        return 0
+      }
+      const find = this.results.filter((item) => item.studentID == studentID && item.index == index)
+      if (find.length > 0){
+        return find[0].number
+      }
+    }
+  },
+  watch: {
+    planIndex(v) {
+      this.planSelected = this.plans[v]
+      this.fetchDataPlanResults()
     }
   }
 }
