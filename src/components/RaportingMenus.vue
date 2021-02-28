b23f26984eb9745e51589afe8e60c20a37b0b87c
unknown <reanyusufali@gmail.com>
2021-02-28 22:23:05 +0700

    :construction: change: router-link to point actual route

--- src/components/RaportingMenus.vue
+++ src/components/RaportingMenus.vue
@@ -5,23 +5,23 @@
         <img src="/img/icon/user-student.svg" class="h-4 sm:h-12">
         <span>Siswa Kelas</span>
       </router-link>
-      <router-link :to="{name: 'raporting.student.exschool'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
+      <router-link :to="{name: 'raporting.exschool.index'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
         <img src="/img/icon/headset.svg" class="h-8 md:h-12">
         <span>Ekstrakurikuler</span>
       </router-link>
-      <router-link :to="{name: 'resulting.plan.result'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
+      <router-link :to="{name: 'raporting.student_note.index'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
         <img src="/img/icon/note.svg" class="h-8 md:h-12">
         <span>Catatan Siswa</span>
       </router-link>
-      <router-link :to="{name: 'resulting.plan.result'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
+      <router-link :to="{name: 'raporting.attendance.index'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
         <img src="/img/icon/binder.svg" class="h-8 md:h-12">
         <span>Absensi Siswa</span>
       </router-link>
-      <router-link :to="{name: 'resulting.plan.result'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
+      <router-link :to="{name: 'raporting.result.index'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
         <img src="/img/icon/sheet.svg" class="h-8 md:h-12">
         <span>Penilaian</span>
       </router-link>
-      <router-link :to="{name: 'resulting.plan.result'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
+      <router-link :to="{name: 'raporting.final.index'}" class="px-2  space-x-2 py-2 border border-gray-300 shadow rounded-lg hover:shadow-xl cursor-pointer flex items-center justify-start bg-white" title="Rencana pembelajaran">
         <img src="/img/icon/zip.svg" class="h-8 md:h-12">
         <span>Raport</span>
       </router-link>
