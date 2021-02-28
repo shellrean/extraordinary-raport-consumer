6241947ad8b85c8c8a5da3c8289f3154e69571ae
unknown <reanyusufali@gmail.com>
2021-02-28 22:25:06 +0700

    :sparkles: add: raporting final route view

---
+++ src/views/raporting/final/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+<script>
+import {
+  vuex_state,
+  vuex_methods,
+  vue_data,
+  vue_computed,
+  vue_methods,
+} from '@/core/module/raporting/final/index'
+export default {
+  data() {
+    return vue_data
+  },
+  computed: {
+    ...vuex_state,
+    ...vue_computed
+  },
+  methods: {
+    ...vuex_methods,
+    ...vue_methods
+  }
+}
+</script>
