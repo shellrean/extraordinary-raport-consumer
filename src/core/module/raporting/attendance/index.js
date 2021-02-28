f74f7d26227e330582c3ff64b0a5f2d27ced626e
unknown <reanyusufali@gmail.com>
2021-02-28 22:24:35 +0700

    :sparkles: add: attendance route view

---
+++ src/core/module/raporting/attendance/index.js
@@ -0,0 +1,61 @@
+import { showSweetError } from '@/core/helper/alert.helper'
+import { mapActions, mapState } from 'vuex'
+
+/**
+ * Vue data
+ * @type {Object}
+ */
+const vue_data = {
+
+}
+
+/**
+ * Vue computed
+ * @type {Object}
+ */
+const vue_computed = {
+
+}
+
+/**
+ * Vue methods
+ * @type {Object}
+ */
+const vue_methods = {
+    showError
+}
+
+/**
+ * Vuex states
+ * @type {Object}
+ */
+const vuex_states = {
+
+}
+
+/**
+ * Vuex methods
+ * @type {Object}
+ */
+const vuex_methods = {
+
+}
+
+/**
+ * Export module let's go
+ */
+export {
+  vue_data,
+  vue_methods,
+  vue_computed,
+  vuex_states,
+  vuex_methods,
+}
+
+/**
+ * Show sweet error
+ * @type vue-method
+ */
+function showError(err) {
+    showSweetError(this, err)
+}
